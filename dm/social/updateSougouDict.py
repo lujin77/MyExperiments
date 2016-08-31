8a2a2feaadd6f29d289f063b5466cb12b6a17623
lujin <syani@qq.com>
2016-08-31 14:33:30 +0800

    搜狗输入法脚本，增加注释

--- dm/social/updateSougouDict.py
+++ dm/social/updateSougouDict.py
@@ -1,4 +1,9 @@
 #! -*- coding:utf-8 -*-
+
+"""
+将搜狗输入法细胞的词，转为补充的专名词典（与jieba已有词典不重复的情况下）
+"""
+
 import sys
 
 reload(sys)
@@ -8,6 +13,7 @@ BASE_DICT_PATH = "dict/dict.txt.big"
 NEW_WORDS_PATH = "dict/new_words.txt"
 OUTPUT_DICT_PATH = "dict/sougo.dict.txt"
 
+# 加载jieba库中已有词典的词
 dict_exist_words = {}
 file_base_dict = open(BASE_DICT_PATH, 'r')
 for line in file_base_dict:
@@ -15,11 +21,13 @@ for line in file_base_dict:
     dict_exist_words[segs[0]] = 1
 file_base_dict.close()
 
+# 遍历搜狗的词，与jieba已有词排重，属于新词的生成新词词典
 new_num = 0;
 new_word_list = []
 file_new_word = open(NEW_WORDS_PATH, 'r')
 for line in file_new_word:
     word = line.strip('\n').strip()
+    # jieba已有词典不重复的，作为补充专名生产搜狗的扩展词典
     if not dict_exist_words.has_key(word):
         new_num = new_num + 1
         new_word_list.append("%s 1 nz\n" % (word))
